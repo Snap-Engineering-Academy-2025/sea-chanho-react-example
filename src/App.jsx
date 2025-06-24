10165679ed60e1cac5b517149485d4f4f9bb657b
ckim146 <ckim1775@gmail.com>
2025-06-24 15:35:53 -0700

    new project

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,36 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import './App.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
 // import characters from './protagonists.json'
 
 function App() {
 
 
   return (
-    <>
-      <Container maxWidth="md" sx={{ mb: 4 }}>
-        <Typography
-          variant="h2"
-          align="center"
-          color="text.primary"
-          sx={{ py: 2 }}
-        >
-          CSS Mystery
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
         </Typography>
-        <Typography
-          variant="h5"
-          align="center"
-          color="text.secondary"
-          sx={{ mx: 10 }}
-        >
-          Why isn't the CSS working?
+        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
         </Typography>
-      </Container>
-
-      <Container maxWidth="lg">
-        <Button
-          variant="contained"
-          // I'm trying to use custom CSS defined in the file App.css,
-          // but it isn't working. Why, and how can I fix it?
-          sx={{backgroundColor: "red"}}
-        >
-          Red Button?
-        </Button>
-      </Container>
-    </>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   )
 }
 
