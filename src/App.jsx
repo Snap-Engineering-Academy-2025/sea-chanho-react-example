427393b9595496ef522badbb23fbf387883b2f5c
ckim146 <ckim1775@gmail.com>
2025-06-24 15:57:43 -0700

    horizontal empty cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import './App.css'
+import * as React from 'react';
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import './App.css'
+import Container from "@mui/material/Container";
 
 // import characters from './protagonists.json'
 
@@ -11,27 +15,89 @@ function App() {
 
 
   return (
-      <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+
+    <>
+      <Container maxWidth="md" sx={{ mb: 4 }}>
+        <Typography
+          variant="h2"
+          align="center"
+          color="text.primary"
+          sx={{ py: 2 }}
+        >
+          CSS Mystery
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+        <Typography
+          variant="h5"
+          align="center"
+          color="text.secondary"
+          sx={{ mx: 10 }}
+        >
+          Why isn't the CSS working?
         </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+      </Container>
+
+      <Container maxWidth="lg">
+        <Button
+          variant="contained"
+          // I'm trying to use custom CSS defined in the file App.css,
+          // but it isn't working. Why, and how can I fix it?
+          className="characterButton"
+        >
+          Red Button?
+        </Button>
+
+
+        <Grid container spacing={2}>
+          <Grid>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/static/images/cards/contemplative-reptile.jpg"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  Lizards are a widespread group of squamate reptiles, with over 6,000
+                  species, ranging across all continents except Antarctica
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   )
 }
 
